b08c7d38b029bae61b83a52c40e1a44f11c07008
Rhiguera <rhiguera75@gmail.com>
2026-02-15 07:30:08 +0100

    Refactor IProductService and IProductRepository to ensure non-nullable return types for Update and GetById methods

--- src/DemoArquitectura.Application/Services/IProductService.cs
+++ src/DemoArquitectura.Application/Services/IProductService.cs
@@ -7,6 +7,6 @@ public interface IProductService
     Task<IEnumerable<ProductDto>> GetAllProductsAsync();
     Task<ProductDto?> GetProductByIdAsync(int id);
     Task<ProductDto> CreateProductAsync(CreateProductDto createProductDto);
-    Task<ProductDto?> UpdateProductAsync(UpdateProductDto updateProductDto);
+    Task<ProductDto> UpdateProductAsync(UpdateProductDto updateProductDto);
     Task<bool> DeleteProductAsync(int id);
 }
